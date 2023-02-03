814255d7ecf49182c0d2e56f008c42fd2457cca1
William Russell <williamrussell@Williams-MBP.lan>
2023-02-02 23:09:12 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(`${name}`);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(`${name}`);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(`${name}`);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(`${name}`);
+}
+
+function appendCat(name){
+    const coolCats = [...cats, `${name}`];
+    return coolCats;
+}
+
+function prependCat(name){
+    const coolerCats = [`${name}`, ...cats];
+    return coolerCats;
+}
+
+function removeLastCat(){
+    const coolestCats = [...cats];
+    return coolestCats.slice(0, 2);
+}
+
+function removeFirstCat(){
+    const coolestCats = [...cats];
+    return coolestCats.slice(1);
+}
+
